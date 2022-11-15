f34b0c95fa431ea19a4d98d1afd63cbbf31ff1a0
ANDnR3s <andresebastian.om@gmail.com>
2022-11-15 11:45:14 -0300

    hashing added

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
+
 
 const app = express();
+
+console.log(md5('123456'));
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -18,8 +21,6 @@ const userSchema = new mongoose.Schema({
 	email: String,
 	password: String
 });
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
 const User = new mongoose.model('User', userSchema);
 
@@ -34,7 +35,7 @@ app.get('/login', (req, res)=>{
 
 app.post('/login', (req, res)=>{
 	const username = req.body.username;
-	const password = req.body.password;
+	const password = md5(req.body.password);
 
 	User.findOne({email: username}, (err, foundUser)=>{
 		if(!err){
@@ -60,7 +61,7 @@ app.get('/register', (req, res)=>{
 app.post('/register', (req, res)=>{
 	const newUser = new User({
 		email: req.body.username,
-		password: req.body.password
+		password: md5(req.body.password)
 	});
 
 	newUser.save( (err)=>{
